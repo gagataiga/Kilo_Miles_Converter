9316fc945a99ac9788fcfe3f5ebf1e70ad68e909
志賀大河 <gaitatg97@gmail.com>
2021-09-14 17:43:45 +0200

    finish

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
 (function () {
+
+  "use strict";
   
   let convertType = "miles";
   let heading = document.querySelector("h1");
   let intro = document.querySelector("p");
-  let answerDiv = docu
+  let answerDiv = document.getElementById("answer");
+  let form = document.querySelector("convert");
+
+  document.addEventListener("keydown", function (e) {
+    let key = e.code;
+    // key code
+    if (key === "KeyK") {
+      convertType = "kilometers";
+      heading.innerHTML = "Kilometer to Miles Converter";
+      intro.innerHTML = "Type in a number of kilometers and click the button to convert the distance to miles";
+      
+    } else if (key === "KeyM") {
+      convertType = "miles";
+      heading.innerHTML = "Miles Converter to Kilometer";
+      intro.innerHTML = "Type in a number of miles and click the button to convert the distance to kilometers";
+    }
+  });
+
+  document.addEventListener("submit", function (event) {
+    
+    event.preventDefault();
+
+    let distance = parseFloat(document.getElementById("distance").value);
+
+    let mileConverted = (distance * 1.609344).toFixed(3);
+    let kiloConverted = (distance * 0.621371).toFixed(3);
+
+    if (distance) {
+      if (convertType == "miles") {
+        answerDiv.innerHTML = `${distance} kilometers convert to ${kiloConverted} miles`;
+      } else if (convertType == "kilometers") {
+        answerDiv.innerHTML = `${distance} miles convert to ${mileConverted} kilometers`;
+        }
+    } else {
+      answerDiv.innerHTML = "<h2>Please provide a number</h2>";
+    }
+  });
 
 })();
